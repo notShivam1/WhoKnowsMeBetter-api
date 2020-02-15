fd90e427603c54d844caf68bc22f45289d865687
MadSadLad <shivam.119966@yahoo.com>
2020-02-15 17:25:54 +0530

    Added error handling middleware

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const routes = require("./routes/quiz.routes");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const urlDB = process.env.URLDB;
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 mongoose.connect(urlDB, { useNewUrlParser: true }, (err, db) => {
   if (err) {
     console.log(err);
   }
-  console.log("DB connected succesfully.")
+  console.log("DB connected succesfully.");
 });
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.send("api working");
-})
+});
 
 app.use("/quizzes", routes);
 
@@ -24,5 +24,12 @@ app.listen(PORT, () => {
   console.log(`working at ${PORT}`);
 });
 
+app.use((err, req, res, next) => {
+  if (req.xhr) {
+    res.status(500).send({ error: "Something failed!" });
+  } else {
+    next(err);
+  }
+});
 
-module.exports =  app;
+module.exports = app;
